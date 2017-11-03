179a1d3ca9fbd2dd22920c25e69a83c98548085b
Addy <Ashepherdking@gmail.com>
2017-11-03 12:36:42 -0600

    fix

--- src/components/FlashCards.js
+++ src/components/FlashCards.js
@@ -44,6 +44,31 @@ class FlashCards extends React.Component {
                               title: 'Card 8',
                               front: 'What is the DOM?',
                               back: 'Document Object Model (DOM) is a platform and language-neutral interface that allows programs and scripts to dynamically access and update the content, structure, and style of a document.'
+                            },
+                            {
+                              title: 'Card 9',
+                              front: 'Are props immutable?',
+                              back: 'Yes'
+                            },
+                            {
+                              title: 'Card 10',
+                              front: 'State should be treated as immutable unless ____ is called?',
+                              back: 'setState'
+                            },
+                            {
+                              title: 'Card 11',
+                              front: 'What does "immutable" mean?',
+                              back: 'Immutable data cannot be changed once created, leading to much simpler application development, no defensive copying, and enabling advanced memoization and change detection techniques with simple logic.'
+                            },
+                            {
+                              title: 'Card 12',
+                              front: 'Persistent Data; what is it?',
+                              back: 'Persistent data presents a mutative API which does not update the data in-place, but instead always yields new updated data.'
+                            },
+                            {
+                              title: 'Card 13',
+                              front: 'What is SPA?',
+                              back: 'A SPA is a single page application.  This means that we never reload the page for the user we just change the content they are viewing.  This is handled by utilizing a client side router.  The most popular router for react is react-router.'
                             }
                           ] 
                 };
